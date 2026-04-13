bebaf3bbc8b95cfd4d8ac41cd8f532f670c1180e
Emiliano Christiansen <emitate@gmail.com>
2026-04-13 13:46:53 +0200

    feat(ts): add public API barrel

---
+++ typescript/src/index.ts
@@ -0,0 +1,20 @@
+export { Client } from "./client.js";
+export { McpError } from "./errors.js";
+export {
+  type ContentItem,
+  type Tool,
+  type ToolAnnotationInfo,
+  type CallToolResult,
+  type Resource,
+  type ResourceTemplate,
+  type ReadResourceResult,
+  type PromptArgument,
+  type Prompt,
+  type PromptMessage,
+  type GetPromptResult,
+  type CompleteResult,
+  type ListResult,
+  type ServerInfo,
+  type ServerCapabilities,
+} from "./types.js";
+export { type ClientOptions, type TlsConfig } from "./auth.js";
